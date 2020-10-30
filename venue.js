76118d294afb5d7ec6a130f42bd32b432aa4c6ed
Nikos Amofa <nikosbright@gmail.com>
2020-10-30 01:06:07 -0400

    added edits to HTML

---
+++ venue.js
@@ -0,0 +1,13 @@
+//a function that loads the landing page into the DOM when loaded with the begin search button
+function landingPage(){
+    return ``
+}
+//event function that changes page to the search page when the begin search button is clicked 
+//function to make get requests to the API 
+//functions that make get requests based on the parameters submitted
+//function to clear the search parameters 
+//function that calls all functions 
+function all(){
+    landingPage()
+}
+$(all)
