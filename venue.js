dcc410a06aa48167e62f05e235b8d9b0acf3f6b3
Nikos Amofa <nikosbright@gmail.com>
2020-11-03 20:17:46 -0500

    added miles to meters in radius input

--- venue.js
+++ venue.js
@@ -1,3 +1,7 @@
+'use strict';
+
+const url = 'https://api.foursquare.com/v2/venues/search'
+
 // page const variables to be rendered 
 //page 1 landing page
 const startPage = `<section class="container" id="js-startPage">
@@ -20,34 +24,40 @@ const startPage = `<section class="container" id="js-startPage">
 
 
 //page 2 search page
-const searchPage = `  <section class="container" id="js-search-page">
+const searchPage = `<section class="container" id="js-search-page">
 <div class="item">
     <form id="js-search-form" class="search-form">
         <h2>The Venue Search App</h2>
 
-        <label for="categories">i'm looking for</label>
-        <select name="categories" id="categories">
-                       <option value="food">food</option>
-                       <option value="resturant">Resturants</option>
-                       <option value="lounge">lounge</option>
-                       <option value="outdoor">outdoors</option>
+        <label for="categories">i'm searching for</label>
+        <select name="categories" id="js-categories">
+                       <option value="4d4b7104d754a06370d81259">Art & Culture</option>
+                       <option value="4d4b7105d754a06374d81259">Restaurants</option>
+                       <option value="4d4b7105d754a06376d81259">Nightlife</option>
+                       <option value="4d4b7105d754a06373d81259">Event</option>
+                       <option value="4d4b7105d754a06377d81259">Outdoors</option>
                    </select>
 
         <label for="location">Near</label>
-        <input type="text" name="location">
+        <input type="text" name="location" id="js-location">
 
-        <label for="radius">Within Miles</label>
-        <input type="number" name="radius"min="1" max="60"> 
+        <label for="radius">Within</label>
+        <input type="number" name="radius"min="1"  max="60" id="js-radius" placeholder="meters"> 
+
+        <label for="options">Options</label>
+        <input type="number" name="options" min="1" max="10" id="js-options" placeholder="10"> 
 
         <input type="submit" id="js-search" value="Search">
     </form>
 </div>
-<div class="item" hidden>
-    <ul id="result-list">
-
-    </ul>
-</div>
-</section>`;
+</section>
+<section>
+    <div class="item" id='js-results' hidden>
+        <ul id="js-result-list">
+    
+        </ul>
+    </div>
+    </section>`;
 
 
 //a function that loads the landing page into the DOM when loaded with the begin search button
@@ -58,7 +68,7 @@ function landingPage() {
 
 //Event function that changes HTML content  and renders the search page when the begin search button is clicked 
 function changePage() {
-    $('#js-main-body').on('submit', '#js-start-form', event => {
+    $('#js-main-body').on('click', '#js-start-form', event => {
         event.preventDefault();
         console.log('test');
         $('#js-main-body').html(searchPage)
@@ -66,20 +76,85 @@ function changePage() {
 }
 
 //function that listens to search submissions
-function venueSearch(event){
-    $('#js-main-body').on('submmit','#js-search-form', event =>{
-        console.log('test search')
-    })
+function venueSearch() {
+    $('#js-main-body').on('submit', '.search-form', event => {
+        event.preventDefault();
+        const place = $('#js-location').val()
+        const withinRadius = Number($('#js-radius').val())*1609.344;
+        const category =$('#js-categories').val();
+        const numOfOptions = $('#js-options').val();
+        console.log(place, withinRadius, category, numOfOptions);
+        getVenues(category, place, withinRadius, numOfOptions)
 
+    })
 }
+
+
+// function to format query
+function queryFormat(params) {
+    const queryItems = Object.keys(params).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`);
+    return queryItems.join('&');
+}
+
+
 //function to make get requests to the API 
-//functions that make get requests based on the parameters submitted
+function getVenues(category, place, withinRadius, numOfOptions) {
+
+    const params = {
+        near: place,
+        limit: numOfOptions,
+        v: 20201020,
+        radius: withinRadius,
+        categoryId: category,
+        'client_id': `UUOCIAQLG2H1U0BBI1XWV3CX0T25YDKRHIRK2YDB41KHAD52`,
+        'client_secret': `SR35X0IUEXOBKQHF5J1M5VXNCKQTKI45RVFHB0ZQBBF4TE2P`
+    };
+    const queryString = queryFormat(params);
+    const searchUrl = url + '?' + queryString;
+    console.log(searchUrl);
+    fetch(searchUrl)
+        .then(response => {
+            if (response.ok) {
+                return response.json()
+            }
+            else { throw new Error(response.statusText) }
+        })
+        .then(responseJson => displayResults(responseJson))
+        .catch(error => console.log(error));
+};
+
+
+// function to display results and fetches results photos from the GET venue photos endpoint.
+function displayResults(responseJson) {
+
+    $('#js-result-list').empty();
+    if (responseJson.response.venues.length == 0 || responseJson.response.venues.length == [ ]) {
+        $('#js-result-list').append(
+            `<li>            
+            <p>No results</p>
+            </li>`)
+    }
+    else {
+        for (let i = 0; i < responseJson.response.venues.length; i++) {
+
+            $('#js-result-list').append(
+                `<li>   
+            <h3> ${responseJson.response.venues[i].name}</h3>
+            <img src="#" alt="Picture of venue">
+            <p>description</p>
+            <p>${responseJson.response.venues[i].location.formattedAddress}</p>
+            </hr>
+            </li>`);
+        }
+        $('#js-results').removeAttr('hidden');
+    }
+};
 //function to clear the search parameters 
 //callback function that calls all functions 
 
 function all() {
     landingPage();
     changePage();
-
+    venueSearch();
 };
 $(all);
