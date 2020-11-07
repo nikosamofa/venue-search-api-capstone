38f7dcaf239ea99ecf4d586bc91451ec48511eb3
Nikos Amofa <nikosbright@gmail.com>
2020-11-07 16:20:44 -0500

    corrected error in intro

--- venue.js
+++ venue.js
@@ -9,11 +9,12 @@ const startPage = `<section class="container" id="js-startPage">
 <div class="item start-">
     <form id="js-start-form" class="start-form">
         <legend>Venue<br>App.</legend>
-        <p>Are you looking for a location for a wedding, dinner date, a surprise birthday party or a chillout spot for friends and
-            family ? Look no further, this app is designed to helps you finde the perfect venue to experience these
+        <p>Are you looking for a location for a wedding, a dinner date, a surprise birthday party or a chillout spot for friends and
+            family ? Look no further, this app is designed to helps you find the perfect venue to experience these
             cherished moments. With the help of advanced technologies and a vast database of locations within your area, our
             app will help you narrow down to the perfect venue that meets your needs and matches your taste</p>
-        <input type="submit" value="Search" class="js-start-search search-button">
+
+        <input type="submit" value="Start Search" class="js-start-search search-button">
     </form>
 </div>
 </section>`;
@@ -24,7 +25,7 @@ const searchPage =
 `<div class="container">
 <div class="item">
     <form id="js-search-form" class="search-form">
-        <label for="categories">Search you're Venue</label>
+        <label for="categories">Search your Venue</label>
         <select name="categories" id="js-categories" >
             <option value="4f4528bc4b90abdf24c9de85">Sports</option>
             <option value="4d4b7104d754a06370d81259">Art & Culture</option>
@@ -37,7 +38,7 @@ const searchPage =
         <label for="location">City</label>
         <input type="text" name="location" id="js-location" placeholder="New York" value="New York">
 
-        <label for="radius">Within</label>
+        <label for="radius">Radius of</label>
         <input type="number" name="radius"min="1"  max="60" id="js-radius" placeholder="miles" value="2"> 
 
         <label for="options">Options</label>
@@ -45,6 +46,7 @@ const searchPage =
 
         <input type="submit" id="js-search" value="Search">
     </form>
+
 </div>
 
 <div class="result-list item-double" id='js-results' hidden>
